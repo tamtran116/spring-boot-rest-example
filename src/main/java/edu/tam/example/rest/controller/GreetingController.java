969e3f4b5b003f4fc1afc10eb6404b496cb8ed29
tamtran116 <thtt116@me.com>
2018-07-29 20:10:39 -0500

    init commit

---
+++ src/main/java/edu/tam/example/rest/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package edu.tam.example.rest.controller;
+
+import edu.tam.example.rest.resource.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(method = GET, path = "/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
---
+++ src/main/java/edu/tam/example/rest/resource/Greeting.java
@@ -0,0 +1,10 @@
+package edu.tam.example.rest.resource;
+
+import lombok.Data;
+
+@Data
+public class Greeting {
+    private final long id;
+    private final String content;
+
+}
